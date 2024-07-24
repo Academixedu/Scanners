8ad354f4be39c5d3c7a0966ebd8e04832a30ee2d
Manisugguna0 <subrahmanyamanikanta@gmail.com>
2024-07-24 18:04:36 +0530

    MANI

--- BugFix.java
+++ BugFix.java
@@ -1,9 +1,14 @@
+import java.util.Scanner;
+
 public class BugFix{
-Scanner in=new Scanner(System.in);
-  int id;
+
+  
   public static void main(String[]args){
+    Scanner in = new Scanner(System.in);
+    int id;
     BugFix b=new BugFix();
+    System.out.println("Enter id ");
     id=in.nextInt();
-    System.out.println()
+    System.out.println("id is "+id);
 }
 }
