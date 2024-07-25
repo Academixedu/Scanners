f74f6648528eaa9cdf25f1ea2978067343e737f8
12Rameshwar <Chavhanrameshwar13@gmail.com>
2024-07-25 23:04:49 +0530

    Update BugFix.java

--- BugFix.java
+++ BugFix.java
@@ -1,9 +1,14 @@
-public class BugFix{
-Scanner in=new Scanner(System.in);
-  int id;
-  public static void main(String[]args){
-    BugFix b=new BugFix();
-    id=in.nextInt();
-    System.out.println()
+import java.util.Scanner;
+
+public class BugFix {
+    Scanner in = new Scanner(System.in); 
+    int id;
+ public static void main(String[] args) {
+     BugFix b = new BugFix();
+ System.out.print("Enter an ID: ");
+        b.id = b.in.nextInt();
+ System.out.println("Entered ID: " + b.id);
+        b.in.close();
+    }
 }
-}
+
