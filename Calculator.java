302d6a6f9f09ad1a5d11b6b7be8c6f65725da0dd
12Rameshwar <Chavhanrameshwar13@gmail.com>
2024-07-25 23:14:37 +0530

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,9 +1,46 @@
-public class Calculator{
-// prepare function with two paramaters and name it as add and return addition of two paramaters as integer
-// prepare function with two paramaters and name it as sub and return Multiplication of two paramaters as integer
-// prepare function with two paramaters and name it as multi and return Substraction of two paramaters as integer
-// prepare function with two paramaters and name it as division and return Division of two paramaters as integer
-  public static void main(String[]args){
-  // Call that functions
-  } 
+public class Calculator {
+
+    int a;
+    int b;
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+
+    public int sub(int a, int b) {
+        return a * b; 
+    }
+
+
+    public int multi(int a, int b) {
+        return a - b; 
+    }
+
+    
+    public int division(int a, int b) {
+      return a / b;
+    }
+
+    public static void main(String[] args) {
+        
+        Calculator calc = new Calculator();
+
+        int sum = calc.add(10, 5);
+        System.out.println("Addition of 10 and 5: " + sum);
+
+        
+        int product = calc.sub(10, 5);
+        System.out.println("Multiplication of 10 and 5: " + product);
+
+        
+        int difference = calc.multi(10, 5);
+        System.out.println("Subtraction of 5 from 10: " + difference);
+
+    
+        int quotient = calc.division(10, 5);
+        System.out.println("Division of 10 by 5: " + quotient);
+
+        
+    }
 }
+
