1706a691ce7c65eb579ad9bee3e1d83bd961cd2e
Sarveshbramhane <139378858+Sarveshbramhane@users.noreply.github.com>
2024-07-26 00:20:04 +0530

    sarvesh

--- Variables.java
+++ Variables.java
@@ -1,10 +1,40 @@
-public class Varibles{
+import java.util.Scanner;
+public class Variables{
   // Take each Data Type for Each Variable and Initialize them using scanner here
   // id ->int
   // name ->String
   // sal ->double
   // gender ->char
   // Pass ->boolean
+  int id;        
+  String name;  
+  double sal;    
+  char gender;   
+  boolean pass;  
+ public void Initializevar()
+ {
+  Scanner scanner =new Scanner(System.in);
+    
+  System.out.print("Enter ID (int): ");
+  id = scanner.nextInt();
+
+  scanner.nextLine(); 
+
+  System.out.print("Enter Name (String): ");
+  name = scanner.nextLine();
+
+  System.out.print("Enter Salary (double): ");
+  sal = scanner.nextDouble();
+
+  System.out.print("Enter Gender (char): ");
+  gender = scanner.next().charAt(0);
+
+  System.out.print("Enter Pass Status (true/false): ");
+  pass = scanner.nextBoolean();
+}
 public static void main(String[]args){
 // Call the varibles in the main method and print them
+Variables a=new Variables();
+a.Initializevar();
+
 }}
