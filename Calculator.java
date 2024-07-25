d8656c3050899b4392b9dbf6c51d3659d552e170
fayazshaik515 <fayazshaik231104@gmail.com>
2024-07-25 16:47:30 +0530

    from fayaz

--- Calculator.java
+++ Calculator.java
@@ -1,9 +1,28 @@
 public class Calculator{
-// prepare function with two paramaters and name it as add and return addition of two paramaters as integer
+  // prepare function with two paramaters and name it as add and return addition of two paramaters as integer
+    public int add(int a,int b){
+      return a+b;
+
+  }
 // prepare function with two paramaters and name it as sub and return Multiplication of two paramaters as integer
+    public int sub(int num1,int num2){
+      return num1*num2;
+    }
 // prepare function with two paramaters and name it as multi and return Substraction of two paramaters as integer
+    public int multi(int n1,int n2){
+      return n1-n2;
+    }
 // prepare function with two paramaters and name it as division and return Division of two paramaters as integer
+public int division(int v1,int v2){
+      return v1/v2;
+}
   public static void main(String[]args){
   // Call that functions
+  Calculator cal = new Calculator();
+  System.out.println("addition is:"+cal.add(2,3));
+  System.out.println("substitution is:"+cal.sub(6,3));
+  System.out.println("multiplication is:"+cal.multi(2,6));
+  System.out.println("division is:"+cal.division(20,2));
+
   } 
 }
--- Variables.java
+++ Variables.java
@@ -1,10 +1,35 @@
-public class Varibles{
+import java.util.Scanner;
+public class Variables{
   // Take each Data Type for Each Variable and Initialize them using scanner here
   // id ->int
   // name ->String
   // sal ->double
   // gender ->char
   // Pass ->boolean
+  int id;
+  String name;
+  double salary;
+  char gender;
+  boolean pass;
+  public void var(){
+  Scanner sc = new Scanner(System.in);
+  System.out.println("enter id:");
+  int a =sc.nextInt();
+  System.out.println("enter name:");
+  String n =sc.next();
+  System.out.println("enter salary:");
+  double s =sc.nextDouble();
+  System.out.println("enter gender:");
+  char g =sc.next().charAt(0);
+  System.out.println("enter pass:");
+  boolean p =sc.hasNextBoolean();
+  }
+
+
+  
 public static void main(String[]args){
 // Call the varibles in the main method and print them
-}}
+Variables f = new Variables();
+f.var();
+}
+}
