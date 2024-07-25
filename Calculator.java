72f9c288e660a881fe061fee0af5da289e0f2e87
jangamswarupa-595 <swarupajangam9@gmail.com>
2024-07-25 15:31:23 +0530

    Assignment9

--- Calculator.java
+++ Calculator.java
@@ -1,9 +1,37 @@
+
+
 public class Calculator{
 // prepare function with two paramaters and name it as add and return addition of two paramaters as integer
 // prepare function with two paramaters and name it as sub and return Multiplication of two paramaters as integer
 // prepare function with two paramaters and name it as multi and return Substraction of two paramaters as integer
 // prepare function with two paramaters and name it as division and return Division of two paramaters as integer
+public static int add(int a,int b){
+  int s=a+b;
+  return s;
+
+}
+public static int sub(int a,int b){
+ int s=a-b;
+ return s;
+
+}
+public static int multi(int a,int b){
+  int s= a*b;
+  return s;
+
+}
+public static int division(int a,int b){
+  int s= a/b;
+  return s;
+
+}
+
   public static void main(String[]args){
   // Call that functions
+  Calculator c=new Calculator();
+   System.out.println(c.add(4,7));
+   System.out.println(c.sub(1,6));
+   System.out.println(c.multi(1,6));
+   System.out.println(c.division(60,6));
   } 
 }
--- ScanArr.java
+++ ScanArr.java
@@ -1,7 +1,25 @@
+import java.util.Scanner;
+
 public class ScanArr{
 // prepare a function with return type int[]
+public int[] arr(int a[])
+{
+  return a;
+}
   public static void main(String[]args){
 // Call the Function here and initialize an Array using scanner
-    
-  }
+    Scanner scanner = new Scanner(System.in);
+     System.out.print("Enter the value of the array");
+     int value = scanner.nextInt();
+     int[] arr = new int[value];
+     for (int i = 0; i < value; i++){
+      arr[i] = scanner.nextInt();
+         }
+         ScanArr s = new ScanArr();
+         int[] resultArray = s.arr(arr);
+     for (int j: resultArray) {
+      System.out.print(j);
+
+}
+}
 }
