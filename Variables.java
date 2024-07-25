ef9d024dea1c8363e100ab0e94858aa013414b05
psvsai <pasupuletisvsm245@gmail.com>
2024-07-25 21:23:08 +0530

    psvsai

--- Variables.java
+++ Variables.java
@@ -1,4 +1,6 @@
-public class Varibles{
+import java.util.Scanner;
+
+public class Variables{
   // Take each Data Type for Each Variable and Initialize them using scanner here
   // id ->int
   // name ->String
@@ -7,4 +9,26 @@ public class Varibles{
   // Pass ->boolean
 public static void main(String[]args){
 // Call the varibles in the main method and print them
+     int id ;
+     String name ;
+     double sal ;
+     char gender ;
+     boolean pass ;
+     Scanner sc = new Scanner(System.in);
+     id = sc.nextInt();
+     name =sc.next();
+     sal = sc.nextDouble();
+     gender = sc.next().charAt(0);
+     pass = sc.nextBoolean();
+     System.out.println("Emp id:   " + id);
+     System.out.println("Name of Emp:   " + name);
+     System.out.println("Salary of Emp:  " + sal);
+     System.out.println("Gender of Emp:   " + gender);
+     System.out.println("Emp:  " + pass);
+
+     
+
+
+
+     
 }}
