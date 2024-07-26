e38082ac265ea48b6931b808f682d2bba695c0ef
preetham0223 <sagilinoyal@gmail.com>
2024-07-26 14:54:40 +0530

    commit

--- ScanArr.java
+++ ScanArr.java
@@ -1,7 +1,22 @@
+import java.util.Scanner;
 public class ScanArr{
 // prepare a function with return type int[]
+public static int[] function(){
+  return null;
+  
+}
   public static void main(String[]args){
 // Call the Function here and initialize an Array using scanner
-    
+System.out.println("enter a");
+ Scanner b = new Scanner(System.in);
+ int a=b.nextInt();
+ int Array[]=new int[a];
+ for(int i=0;i<Array.length;i++){
+  System.out.println("enter array:"+i);
+  Array[i]=b.nextInt();
+ }    
+ for (int j :Array){
+  System.out.println(j);
+ }
   }
 }
